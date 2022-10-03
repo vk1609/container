cd3890605b241b80a0f43d92fddb2eba56d650b6
tarunbylapudi <92293379+tarunbylapudi@users.noreply.github.com>
2022-10-03 15:15:52 +0530

    added email service

---
+++ contact-email.js
@@ -0,0 +1,20 @@
+function sendMail() {
+  var templateParams = {
+    name: document.getElementById("first-name").value,
+    email: document.getElementById("email").value,
+     message : document.getElementById("message").value
+  };
+
+  emailjs.send('service_a4wyg6o', 'template_o44w8sa', templateParams)
+    .then(function (response) {
+      console.log('SUCCESS!', response.status, response.text);
+      alert("Thank you for reaching out, We'll get back to you as soon as possible!")
+      window.location.href = "home.html"
+    }, function (error) {
+      console.log('FAILED...', error);
+      alert("There was an error, please try again later!")
+      window.location.href = "home.html"
+
+    });
+
+}
---
+++ register-email.js
@@ -0,0 +1,20 @@
+function sendMail() {
+  var templateParams = {
+    name: document.getElementById("first-name").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value
+  };
+
+  emailjs.send('service_a4wyg6o', 'template_gnm504z', templateParams)
+    .then(function (response) {
+      console.log('SUCCESS!', response.status, response.text);
+      alert("Signup successful!, Credentials sent to your registered email address.")
+      window.location.href = "home.html"
+    }, function (error) {
+      console.log('FAILED...', error);
+      alert("There was an error, please try again later!")
+      window.location.href = "home.html"
+
+    });
+
+}
